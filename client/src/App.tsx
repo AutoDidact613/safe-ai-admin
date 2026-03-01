78331b75033c509ac2f5d5d68b59bba04d545712
ayala-a11y <ayalakubashka@gmail.com>
2026-03-01 03:57:41 +0200

    Clean up CSS imports in App.tsx
    Removed unused CSS import and retained design system styles.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
-<<<<<<< feature/Tasks
 import { useState } from 'react'
-// import "./styles/openai-common.css";
-=======
 import "./styles/design-system.css";
->>>>>>> main
 import './App.css'
 import AppRouter from './router/AppRouter'
 
